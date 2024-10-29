71fa4c76c1f309131dec34bcebf3918760607835
Matt Chang <echoedinvoker@gmail.com>
2024-10-30 01:29:24 +0800

    Remove Button and replace with divs in NotodoShowPage

--- src/app/[userId]/notodo/[notodoId]/page.tsx
+++ src/app/[userId]/notodo/[notodoId]/page.tsx
@@ -1,6 +1,5 @@
 import { db } from "@/db";
 import Link from "next/link";
-import { Button } from "@nextui-org/react";
 import NotodoShowActions from "@/components/notodo/notodo-show-actions";
 import { paths } from "@/paths";
 
@@ -15,7 +14,6 @@ export default async function NotodoShowPage({ params: { notodoId, userId } }: N
   const notodo = await db.notodo.findFirst({
     where: { id: notodoId }
   });
-
   if (!notodo) {
     return <div>Notodo not found</div>
   }
@@ -24,16 +22,21 @@ export default async function NotodoShowPage({ params: { notodoId, userId } }: N
     <Link href={paths.notodoListPage(userId)}>
       <h1 className="text-xl font-bold text-stone-700">{notodo.title}</h1>
     </Link>
+    <div>
+      <p className="text-stone-600 text-sm whitespace-pre-line">{notodo.content}</p>
+    </div>
     <div className="grid grid-cols-4 gap-4">
       <div className="col-span-3">
-        <div className="rounded-lg py-2 px-4 shadow hover:shadow-md transition duration-300 text-stone-700 bg-stone-50 mb-4">
-          <Link href={paths.thresholdListPage(userId, notodoId)}>
-            <Button>Thresholds</Button>
+        <div className="flex gap-4">
+          <Link className="flex-1" href={paths.thresholdListPage(userId, notodoId)}>
+            <div className="rounded-lg py-2 px-4 shadow hover:shadow-md transition duration-300 text-stone-700 bg-stone-50 mb-4">
+              <h3>Thresholds</h3>
+            </div>
           </Link>
-        </div>
-        <div className="rounded-lg py-2 px-4 shadow hover:shadow-md transition duration-300 text-stone-700 bg-stone-50 mb-4">
-          <Link href={paths.challengeListPage(userId, notodoId)}>
-            <Button>Challenges</Button>
+          <Link className="flex-1" href={paths.challengeListPage(userId, notodoId)}>
+            <div className="rounded-lg py-2 px-4 shadow hover:shadow-md transition duration-300 text-stone-700 bg-stone-50 mb-4">
+              <h3>Challenges</h3>
+            </div>
           </Link>
         </div>
       </div>
