68848fb338b8e35fa781e4b15f483ce04da0181a
Matt Chang <echoedinvoker@gmail.com>
2024-11-04 20:41:22 +0800

    Add weight field to NotodoForm component

--- src/components/notodo/notodo-form.tsx
+++ src/components/notodo/notodo-form.tsx
@@ -1,11 +1,13 @@
 import { useFormState } from "react-dom";
 import FormButton from "../common/FormButton";
-import { Input, Textarea } from "@nextui-org/react";
+import { Checkbox, Input, Textarea } from "@nextui-org/react";
+import { useState } from "react";
 
 interface NotodoFormState {
   errors: {
     title?: string[];
     content?: string[];
+    weight?: string[];
     _form?: string[];
   };
 }
@@ -20,17 +22,19 @@ interface NotodoFormProps {
   defaultValues?: {
     title: string;
     content: string;
+    weight?: number;
   };
 }
 
 
 export default function NotodoForm({ formStateAction, defaultValues }: NotodoFormProps) {
   const [formState, action] = useFormState(formStateAction, { errors: {} });
+  const [isWeightEnabled, setIsWeightEnabled] = useState(defaultValues?.weight ? true : false);
 
   return (
     <form action={action}>
       <div className="flex flex-col gap-4 p4 w-80">
-        <h3 className="text-lg text-stone-700"> a Notodo</h3>
+        <h3 className="text-lg text-stone-700">{defaultValues ? "Edit" : "Create"} a Notodo</h3>
         <Input
           name='title'
           label="Title"
@@ -50,6 +54,22 @@ export default function NotodoForm({ formStateAction, defaultValues }: NotodoFor
           errorMessage={formState?.errors?.content?.join(", ")}
           defaultValue={defaultValues?.content}
         />
+        <Checkbox isSelected={isWeightEnabled} onChange={() => setIsWeightEnabled(!isWeightEnabled)}>
+          Enable Weight
+        </Checkbox>
+        {isWeightEnabled && (
+          <Input
+            name='weight'
+            type="number"
+            label="Weight"
+            labelPlacement="outside"
+            placeholder="Enter the notodo weight"
+            isInvalid={!!formState?.errors?.weight}
+            errorMessage={formState?.errors?.weight?.join(", ")}
+            defaultValue={defaultValues?.weight?.toString() || "1.0"}
+            step="0.1"
+          />
+        )}
         {formState?.errors?._form && (
           <div className="text-red-500">{formState.errors._form.join(", ")}</div>
         )}
