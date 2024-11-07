45f3996944f12638834675fda71bd63bf03d7edb
Matt Chang <echoedinvoker@gmail.com>
2024-11-07 13:45:34 +0800

    Update table schema to use column name "weight" instead of "multiplier".

---
+++ prisma/migrations/20241107054453_remove_weight_default_value/migration.sql
@@ -0,0 +1,19 @@
+-- RedefineTables
+PRAGMA defer_foreign_keys=ON;
+PRAGMA foreign_keys=OFF;
+CREATE TABLE "new_Threshold" (
+    "id" TEXT NOT NULL PRIMARY KEY,
+    "title" TEXT NOT NULL,
+    "content" TEXT NOT NULL,
+    "duration" INTEGER NOT NULL,
+    "weight" REAL NOT NULL,
+    "notodoId" TEXT NOT NULL,
+    "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" DATETIME NOT NULL,
+    CONSTRAINT "Threshold_notodoId_fkey" FOREIGN KEY ("notodoId") REFERENCES "Notodo" ("id") ON DELETE CASCADE ON UPDATE CASCADE
+);
+INSERT INTO "new_Threshold" ("content", "createdAt", "duration", "id", "notodoId", "title", "updatedAt", "weight") SELECT "content", "createdAt", "duration", "id", "notodoId", "title", "updatedAt", "weight" FROM "Threshold";
+DROP TABLE "Threshold";
+ALTER TABLE "new_Threshold" RENAME TO "Threshold";
+PRAGMA foreign_keys=ON;
+PRAGMA defer_foreign_keys=OFF;
