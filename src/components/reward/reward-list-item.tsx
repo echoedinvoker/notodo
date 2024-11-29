d6b3064764f118636dad1c4539f87ea69243c29c
Matt Chang <echoedinvoker@gmail.com>
2024-11-29 19:26:06 +0800

    Refactor reward-list-item component for better readability

--- src/components/reward/reward-list-item.tsx
+++ src/components/reward/reward-list-item.tsx
@@ -12,13 +12,29 @@ export default function RewardListItem({ userId, reward, totalScore }: RewardLis
   const consumabled = totalScore >= reward.pointCost;
 
   return (
-    <div className={`rounded-lg py-2 px-4 shadow transition duration-300 bg-stone-50 ${consumabled ? 'hover:shadow-md text-stone-700' : 'text-stone-500'}`}>
-      <Link href={paths.rewardShowPage(userId, reward.id)}>
-        <div className="flex justify-between items-center">
-          <h3 className="text-lg font-semibold my-1">{reward.name}</h3>
-          <h3 className="text-lg font-semibold italic">Cost: {reward.pointCost}</h3>
+    <div className={`rounded-lg py-2 px-4 shadow transition duration-300 bg-stone-50 ${consumabled
+      ? 'hover:shadow-md text-stone-700 cursor-pointer'
+      : 'text-stone-500 cursor-not-allowed'
+      }`}>
+      {consumabled ? (
+        <Link href={paths.rewardShowPage(userId, reward.id)}>
+          <RewardContent reward={reward} />
+        </Link>
+      ) : (
+        <div>
+          <RewardContent reward={reward} />
         </div>
-      </Link>
+      )}
+
+    </div>
+  );
+}
+
+function RewardContent({ reward }: { reward: Reward }) {
+  return (
+    <div className="flex justify-between items-center">
+      <h3 className="text-lg font-semibold my-1">{reward.name}</h3>
+      <h3 className="text-lg font-semibold italic">Cost: {reward.pointCost}</h3>
     </div>
   );
 }
