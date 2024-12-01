20bb3d35b1c32dbf05212a7a048bc98f90e40089
Matt Chang <echoedinvoker@gmail.com>
2024-12-02 00:44:27 +0800

    Add API endpoint for fetching breadcrumb segments

---
+++ src/app/api/label/route.ts
@@ -0,0 +1,80 @@
+import { db } from '@/db';
+import { type NextRequest } from 'next/server'
+
+const STATIC_PATH_NAMES = ['notodo', 'create', 'threshold', 'challenge', 'reward']
+ 
+export async function GET(request: NextRequest) {
+  const requestHeaders = new Headers(request.headers)
+  const referer = requestHeaders.get('referer')
+  const host = requestHeaders.get('host')
+
+  if (!referer || !host) {
+    return Response.json({
+      error: 'Referer not found'
+    }, {
+      status: 400
+    });
+  }
+
+  const segments = await getSegmentInfo(referer, host)
+
+  return Response.json({ segments });
+}
+
+
+async function getSegmentInfo(referer: string, host: string) {
+  let tmpReferer = referer
+  const segments: { label: string; href: string }[] = []
+  while (tmpReferer) {
+    const lastSegment = tmpReferer.split('/').at(-1)
+    const lastTwoSegments = tmpReferer.split('/').at(-2)
+    if (lastSegment && STATIC_PATH_NAMES.includes(lastSegment)) {
+      segments.push({
+        label: lastSegment,
+        href: tmpReferer
+      })
+      tmpReferer = tmpReferer.replace(`/${lastSegment}`, '')
+    } else if (lastSegment && lastTwoSegments && STATIC_PATH_NAMES.includes(lastTwoSegments)) {
+      segments.push({
+        label: await getLabelFromDB(lastTwoSegments, lastSegment),
+        href: tmpReferer
+      })
+      tmpReferer = tmpReferer.replace(`/${lastSegment}`, '')
+    } else {
+      segments.push({
+        label: 'Home',
+        href: host
+      })
+      tmpReferer = ''
+    }
+  }
+  return segments
+}
+
+async function getLabelFromDB(pathname: string, id: string) {
+  if (pathname === 'notodo') {
+    const notodo = await db.notodo.findFirst({
+      where: { id }
+    })
+    return notodo ? notodo.title : ''
+  }
+  if (pathname === 'threshold') {
+    const threshold = await db.threshold.findFirst({
+      where: { id }
+    })
+    return threshold ? threshold.title : ''
+  }
+  if (pathname === 'challenge') {
+    const challenge = await db.challenge.findFirst({
+      where: { id }
+    })
+    return challenge ? challenge.id : ''
+  }
+  if (pathname === 'reward') {
+    const reward = await db.reward.findFirst({
+      where: { id }
+    })
+    return reward ? reward.name : ''
+  }
+  return ''
+}
--- src/components/common/breadcrumbs-component.tsx
+++ src/components/common/breadcrumbs-component.tsx
@@ -1,17 +1,34 @@
 'use client';
 
-import { BreadcrumbItem, Breadcrumbs } from "@nextui-org/react";
+import { BreadcrumbItem, Breadcrumbs, Skeleton } from "@nextui-org/react";
 import { usePathname } from "next/navigation";
+import { useEffect, useState } from "react";
 
 type NavItem = {
   href: string;
   label: string;
-  disabled: boolean;
 };
 
 export default function BreadcrumbsComponent() {
   const pathname = usePathname()
-  const navItems = generateNavItems(pathname);
+  const [navItems, setNavItems] = useState<NavItem[]>([]);
+
+
+  useEffect(() => {
+    fetchSomething();
+  }, [pathname])
+
+  async function fetchSomething() {
+    const response = await fetch('/api/label');
+    const { segments } = await response.json();
+    setNavItems(segments.reverse());
+  }
+
+  if (navItems.length === 0) {
+    return (
+      <Skeleton className="w-72 h-[20px]" />
+    )
+  }
 
   return (
     <>
@@ -26,30 +43,4 @@ export default function BreadcrumbsComponent() {
     )}
     </>
   )
-}
-
-function generateNavItems(pathname: string): NavItem[] {
-  const parts = pathname.split('/').filter(Boolean);
-  const navItems: NavItem[] = [];
-  let currentPath = '';
-
-  for (let i = 0; i < parts.length; i++) {
-    currentPath += '/' + parts[i];
-    
-    let label = parts[i];
-    if (i === 0) continue; // Skip the first part (user ID)
-    
-    navItems.push({
-      href: currentPath,
-      label: label,
-      disabled: i === parts.length - 1 // Last item is disabled
-    });
-  }
-
-  // Set all items except the last one to not disabled
-  for (let i = 0; i < navItems.length - 1; i++) {
-    navItems[i].disabled = false;
-  }
-
-  return navItems;
 }
