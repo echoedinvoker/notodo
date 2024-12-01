5f89de5483479d3c5875a7229625510fed378e9d
Matt Chang <echoedinvoker@gmail.com>
2024-12-01 21:51:39 +0800

    Refactor breadcrumb component with dynamic navigation items

--- src/components/common/breadcrumbs-component.tsx
+++ src/components/common/breadcrumbs-component.tsx
@@ -3,14 +3,52 @@
 import { BreadcrumbItem, Breadcrumbs } from "@nextui-org/react";
 import { usePathname } from "next/navigation";
 
+type NavItem = {
+  href: string;
+  label: string;
+  disabled: boolean;
+};
+
 export default function BreadcrumbsComponent() {
   const pathname = usePathname()
+  const navItems = generateNavItems(pathname);
+
+
   return (
-    <Breadcrumbs>
-      <BreadcrumbItem>Test</BreadcrumbItem>
-      <BreadcrumbItem>Test</BreadcrumbItem>
-      <BreadcrumbItem>Test</BreadcrumbItem>
-      <BreadcrumbItem>Test</BreadcrumbItem>
-    </Breadcrumbs>
+    <>
+    {navItems.length > 0 && (
+      <Breadcrumbs>
+      {navItems.map((item) => (
+        <BreadcrumbItem href={item.href}>{item.label}</BreadcrumbItem>
+      ))}
+      </Breadcrumbs>
+    )}
+    </>
   )
+}
+
+function generateNavItems(pathname: string): NavItem[] {
+  const parts = pathname.split('/').filter(Boolean);
+  const navItems: NavItem[] = [];
+  let currentPath = '';
+
+  for (let i = 0; i < parts.length; i++) {
+    currentPath += '/' + parts[i];
+    
+    let label = parts[i];
+    if (i === 0) continue; // Skip the first part (user ID)
+    
+    navItems.push({
+      href: currentPath,
+      label: label,
+      disabled: i === parts.length - 1 // Last item is disabled
+    });
+  }
+
+  // Set all items except the last one to not disabled
+  for (let i = 0; i < navItems.length - 1; i++) {
+    navItems[i].disabled = false;
+  }
+
+  return navItems;
 }
