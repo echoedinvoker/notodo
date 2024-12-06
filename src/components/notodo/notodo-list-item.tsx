d73b86d67367ec7d6d34261dfcac20dfe61cee03
Matt Chang <echoedinvoker@gmail.com>
2024-12-06 16:18:06 +0800

    Update NotodoListItem component with new FormButton component

--- src/components/notodo/notodo-list-item.tsx
+++ src/components/notodo/notodo-list-item.tsx
@@ -1,20 +1,19 @@
 import { NotodoWithData } from "@/db/queries/notodos";
 import { paths } from "@/paths";
 import Link from "next/link";
-import { TheScore } from "../common";
+import { FormButton, TheScore } from "../common";
 import TheHour from "../common/the-hour";
 import ThresholdNext from "../threhold/threshold-next";
 import { calculateNotodoScore } from "@/helpers/utils";
 import { Divider } from "@nextui-org/react";
+import * as actions from "@/actions"
 
 interface NotodoListItemProps {
   notodo: NotodoWithData;
 }
 
 export default function NotodoListItem({ notodo }: NotodoListItemProps) {
-  const activeChallenge = notodo.challenges.find(challenge => !challenge.endTime);
   const { isOngoing, currentScore, currentWeight, nextThreshold } = calculateNotodoScore(notodo);
-
 
   return (
     <div className="rounded-lg py-2 px-4 shadow hover:shadow-md transition duration-300 text-stone-700 bg-stone-50 active:bg-stone-100">
@@ -25,8 +24,8 @@ export default function NotodoListItem({ notodo }: NotodoListItemProps) {
           <h3 className="text-md font-semibold my-1">{notodo.title}</h3>
         </Link >
         <Divider />
-        <div className="text-sm text-stone-500 flex flex-col items-start justify-start">
-          {activeChallenge && isOngoing && (
+        <div className="w-full text-sm text-stone-500 flex flex-col items-start justify-start">
+          {isOngoing ? (
             <>
               <TheHour notodo={notodo} />
               <TheScore totalScore={currentScore!} currentWeight={currentWeight!} />
@@ -35,7 +34,14 @@ export default function NotodoListItem({ notodo }: NotodoListItemProps) {
                 nextThreshold={nextThreshold}
               />
             </>
+          ) : (
+            <form action={actions.startChallenge.bind(null, notodo.id)} className="w-full h-28 flex justify-center items-center">
+              <FormButton className="w-full h-full bg-opacity-0 hover:drop-shadow-md active:drop-shadow-none transition duration-250 p-2 text-stone-400 uppercase rounded-md font-mono font-semibold">
+                Start New Challenge!
+              </FormButton>
+            </form>
           )}
+
         </div>
       </div>
     </div >
