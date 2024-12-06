7b3216764a8a9583c0644fff72f7de157f1061f6
Matt Chang <echoedinvoker@gmail.com>
2024-12-06 17:03:55 +0800

    Refactor NotodoListItem component UI and functionality

--- src/components/notodo/notodo-list-item.tsx
+++ src/components/notodo/notodo-list-item.tsx
@@ -15,29 +15,35 @@ interface NotodoListItemProps {
 export default function NotodoListItem({ notodo }: NotodoListItemProps) {
   const { isOngoing, currentScore, currentWeight, nextThreshold } = calculateNotodoScore(notodo);
 
+  console.log({ isOngoing, currentScore, currentWeight, nextThreshold })
+
   const wrappedAction = async () => {
     "use server";
     await actions.startChallenge(notodo.id)
   }
 
   return (
-    <div className="rounded-lg py-2 px-4 shadow hover:shadow-md transition duration-300 text-stone-700 bg-stone-50 active:bg-stone-100">
-      <div className="flex flex-col justify-start items-start">
+    <div className="rounded-lg py-3 px-5 shadow hover:shadow-md transition duration-300 text-stone-700 bg-stone-50 active:bg-stone-100">
+      <div className="h-full flex flex-col justify-start items-start">
         <Link
           className="hover:drop-shadow-md transition active:text-stone-500"
           href={paths.notodoShowPage(notodo.user.id, notodo.id)}>
           <h3 className="text-md font-semibold my-1">{notodo.title}</h3>
         </Link >
-        <Divider />
-        <div className="w-full text-sm text-stone-500 flex flex-col items-start justify-start">
+        <Divider className="my-1" />
+        <div className="w-full text-sm text-stone-500 flex flex-col items-start justify-start gap-1">
           {isOngoing ? (
             <>
               <TheHour notodo={notodo} />
-              <TheScore totalScore={currentScore!} currentWeight={currentWeight!} />
-              <ThresholdNext
-                notodo={notodo}
-                nextThreshold={nextThreshold}
-              />
+              {currentWeight && (
+                <>
+                  <TheScore totalScore={currentScore!} currentWeight={currentWeight!} />
+                  <ThresholdNext
+                    notodo={notodo}
+                    nextThreshold={nextThreshold}
+                  />
+                </>
+              )}
             </>
           ) : (
             <form action={wrappedAction} className="w-full h-28 flex justify-center items-center">
@@ -46,8 +52,14 @@ export default function NotodoListItem({ notodo }: NotodoListItemProps) {
               </FormButton>
             </form>
           )}
-
         </div>
+        {isOngoing && !currentWeight && (
+          <form className="w-full h-full flex items-center justify-center">
+            <FormButton className="w-full h-full bg-opacity-0 hover:drop-shadow-md active:drop-shadow-none transition duration-250 p-2 text-stone-300 hover:text-stone-400 uppercase rounded-md font-mono font-semibold">
+              No score yet, enable it?
+            </FormButton>
+          </form>
+        )}
       </div>
     </div >
   );
