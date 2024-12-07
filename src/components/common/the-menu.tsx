bdf0ceab6bbaf67254add9fb1d5c9e129a174144
Matt Chang <echoedinvoker@gmail.com>
2024-12-07 20:02:32 +0800

    Update link prefetching in TheMenu component

--- src/components/common/the-menu.tsx
+++ src/components/common/the-menu.tsx
@@ -12,8 +12,8 @@ export default function TheMenu({ userId }: TheMenuProps) {
       <ToggleMenu className="absolute right-3 -top-1 peer z-30 sm:hidden" />
       <div className="fixed inset-0 hidden peer-has-[:checked]:block sm:peer-has-[:checked]:hidden bg-black/30 z-20 backdrop-blur-sm sm:hidden"></div>
       <div className="mt-[250px] mb-[500px] hidden peer-has-[:checked]:flex sm:peer-has-[:checked]:hidden flex-col gap-12 justify-start items-center fixed inset-0 z-20 screen-w screen-h">
-        <Link className="hover:underline uppercase font-semibold tracking-wide" href={paths.createNotodoPage(userId)}>create</Link>
-        <Link className="hover:underline uppercase font-semibold tracking-wide" href={paths.rewardListPage(userId)}>consume</Link>
+        <Link className="hover:underline uppercase font-semibold tracking-wide" href={paths.createNotodoPage(userId)} prefetch>create</Link>
+        <Link className="hover:underline uppercase font-semibold tracking-wide" href={paths.rewardListPage(userId)} prefetch>consume</Link>
       </div>
     </>
   )
