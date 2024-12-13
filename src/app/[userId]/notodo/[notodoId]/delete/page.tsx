91da4739fe29b03113fa0b94b5334d0bf94a3788
Matt Chang <echoedinvoker@gmail.com>
2024-12-13 17:15:51 +0800

    Update styling class for padding in delete page component

--- src/app/[userId]/notodo/[notodoId]/delete/page.tsx
+++ src/app/[userId]/notodo/[notodoId]/delete/page.tsx
@@ -10,7 +10,7 @@ interface EditNotodoPageProps {
 export default async function EditNotodoPage({ params: { notodoId } }: EditNotodoPageProps) {
 
   return (
-    <div className="p-12 flex flex-col gap-6 items-start justify-around">
+    <div className="py-12 flex flex-col gap-6 items-start justify-around">
       <form action={actions.deleteNotodo.bind(null, notodoId)} className="w-full text-center">
         <FormButton className="uppercase font-semibold min-w-[300px] hover:bg-red-500 hover:text-white">
           Delete and remove effect
