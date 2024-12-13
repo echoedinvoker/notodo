b7aacfc7eebac285dc99035a7d7644e3a4505323
Matt Chang <echoedinvoker@gmail.com>
2024-12-13 17:18:59 +0800

    Update label style in ToggleMenu component

--- src/components/common/toggle-menu.tsx
+++ src/components/common/toggle-menu.tsx
@@ -6,7 +6,7 @@ export default function ToggleMenu({ ...props }: ToggleMenuProps) {
   return (
     <div className="" {...props}>
       <input type="checkbox" className="peer hidden" id="menu-toggle" />
-      <label htmlFor="menu-toggle" className="before:content-['☰'] peer-checked:before:content-['✕']"></label>
+      <label htmlFor="menu-toggle" className="before:content-['☰'] peer-checked:before:content-['✕'] before:text-lg"></label>
     </div>
   )
 }
