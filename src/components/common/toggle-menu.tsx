c814b1b1ba90006d0db8b4ac63c641e97049e0a0
Matt Chang <echoedinvoker@gmail.com>
2024-12-13 17:27:46 +0800

    Update menu toggle styles for better user experience

--- src/components/common/toggle-menu.tsx
+++ src/components/common/toggle-menu.tsx
@@ -6,7 +6,10 @@ export default function ToggleMenu({ ...props }: ToggleMenuProps) {
   return (
     <div className="" {...props}>
       <input type="checkbox" className="peer hidden" id="menu-toggle" />
-      <label htmlFor="menu-toggle" className="before:content-['☰'] peer-checked:before:content-['✕'] before:text-lg"></label>
+      <label
+        htmlFor="menu-toggle"
+        className="before:content-['☰'] peer-checked:before:content-['✕'] before:text-lg before:w-8 before:h-8 before:rounded-full before:flex before:items-center before:justify-center hover:before:bg-stone-200 before:duration-200"
+      ></label>
     </div>
   )
 }
