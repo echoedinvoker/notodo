169bcd1f7933bea4842a02059214d05ba02f6d31
Matt Chang <echoedinvoker@gmail.com>
2024-12-20 17:40:19 +0800

    Add RewardListItemPressingBar component for progress bar

---
+++ src/components/reward/reward-list-item-pressing-bar.tsx
@@ -0,0 +1,54 @@
+'use client';
+
+import { useEffect, useState } from "react";
+
+export default function RewardListItemPressingBar({ consumabled }: { consumabled: boolean }) {
+  const [isHolding, setIsHolding] = useState(false);
+  const [progress, setProgress] = useState(0);
+
+  useEffect(() => {
+    let interval: NodeJS.Timeout;
+    if (isHolding && consumabled) {
+      interval = setInterval(() => {
+        setProgress((prev) => {
+          if (prev >= 100) {
+            clearInterval(interval);
+            return 100;
+          }
+          return prev + 2; // 調整這個值來改變進度條填充的速度
+        });
+      }, 50);
+    } else {
+      setProgress(0);
+    }
+    return () => clearInterval(interval);
+  }, [isHolding, consumabled]);
+
+  const handleMouseDown = () => {
+    if (consumabled) {
+      setIsHolding(true);
+    }
+  };
+
+  const handleMouseUp = () => {
+    setIsHolding(false);
+  };
+
+  const handleMouseLeave = () => {
+    setIsHolding(false);
+  };
+  return (
+    <div
+      className="z-10 w-full absolute left-0 top-0 bottom-0 rounded-lg transition-all duration-50 ease-linear"
+      onMouseDown={handleMouseDown}
+      onMouseUp={handleMouseUp}
+      onMouseLeave={handleMouseLeave}
+    >
+      <div
+        className="h-full bg-blue-500 opacity-30 transition-all duration-50 ease-linear rounded-lg"
+        style={{ width: `${progress}%` }}
+      />
+
+    </div>
+  );
+}
--- src/components/reward/reward-list-item.tsx
+++ src/components/reward/reward-list-item.tsx
@@ -1,8 +1,6 @@
-'use client';
-
 import type { Reward } from "@prisma/client";
-import { useEffect, useState } from "react";
 import { FaCoins } from "react-icons/fa";
+import RewardListItemPressingBar from "./reward-list-item-pressing-bar";
 
 interface RewardListItemProps {
   userId: string;
@@ -12,54 +10,14 @@ interface RewardListItemProps {
 
 export default function RewardListItem({ userId, reward, totalScore }: RewardListItemProps) {
   const consumabled = totalScore >= reward.pointCost;
-  const [isHolding, setIsHolding] = useState(false);
-  const [progress, setProgress] = useState(0);
-
-  useEffect(() => {
-    let interval: NodeJS.Timeout;
-    if (isHolding && consumabled) {
-      interval = setInterval(() => {
-        setProgress((prev) => {
-          if (prev >= 100) {
-            clearInterval(interval);
-            return 100;
-          }
-          return prev + 2; // 調整這個值來改變進度條填充的速度
-        });
-      }, 50);
-    } else {
-      setProgress(0);
-    }
-    return () => clearInterval(interval);
-  }, [isHolding, consumabled]);
-
-  const handleMouseDown = () => {
-    if (consumabled) {
-      setIsHolding(true);
-    }
-  };
-
-  const handleMouseUp = () => {
-    setIsHolding(false);
-  };
-
-  const handleMouseLeave = () => {
-    setIsHolding(false);
-  };
 
   return (
-    <div className={`rounded-lg py-2 px-4 transition duration-300 bg-stone-50 ${consumabled
+    <div className={`relative z-0 rounded-lg py-2 px-4 transition duration-300 bg-stone-50 ${consumabled
       ? 'hover:shadow-lg hover:-translate-y-0.5 active:translate-y-0 text-stone-700 cursor-pointer'
       : 'shadow-inner text-stone-500 cursor-not-allowed'
       }`}
-      onMouseDown={handleMouseDown}
-      onMouseUp={handleMouseUp}
-      onMouseLeave={handleMouseLeave}
     >
-      <div
-        className="absolute left-0 top-0 bottom-0 bg-stone-500/30 rounded-lg transition-all duration-50 ease-linear"
-        style={{ width: `${progress}%` }}
-      ></div>
+      <RewardListItemPressingBar consumabled={consumabled} />
       <RewardContent reward={reward} />
     </div>
   );
