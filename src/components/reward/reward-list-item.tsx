cfaa97ec469244ce015bbbc2cfdea042a5a2bbd5
Matt Chang <echoedinvoker@gmail.com>
2024-12-20 17:09:52 +0800

    Update reward list item UI and remove unused imports

--- src/components/reward/reward-list-item.tsx
+++ src/components/reward/reward-list-item.tsx
@@ -1,6 +1,5 @@
-import { paths } from "@/paths";
 import type { Reward } from "@prisma/client";
-import Link from "next/link";
+import { FaCoins } from "react-icons/fa";
 
 interface RewardListItemProps {
   userId: string;
@@ -11,21 +10,13 @@ interface RewardListItemProps {
 export default function RewardListItem({ userId, reward, totalScore }: RewardListItemProps) {
   const consumabled = totalScore >= reward.pointCost;
 
-  return (
-    <div className={`rounded-lg py-2 px-4 shadow transition duration-300 bg-stone-50 ${consumabled
-      ? 'hover:shadow-md text-stone-700 cursor-pointer'
-      : 'text-stone-500 cursor-not-allowed'
-      }`}>
-      {consumabled ? (
-        <Link href={paths.rewardShowPage(userId, reward.id)}>
-          <RewardContent reward={reward} />
-        </Link>
-      ) : (
-        <div>
-          <RewardContent reward={reward} />
-        </div>
-      )}
 
+  return (
+    <div className={`rounded-lg py-2 px-4 transition duration-300 bg-stone-50 ${consumabled
+      ? 'hover:shadow-lg hover:-translate-y-0.5 active:translate-y-0 text-stone-700 cursor-pointer'
+      : 'shadow-inner text-stone-500 cursor-not-allowed'
+      }`}>
+      <RewardContent reward={reward} />
     </div>
   );
 }
@@ -34,7 +25,14 @@ function RewardContent({ reward }: { reward: Reward }) {
   return (
     <div className="flex justify-between items-center">
       <h3 className="text-lg font-semibold my-1">{reward.name}</h3>
-      <h3 className="text-lg font-semibold italic">Cost: {reward.pointCost}</h3>
+      <p className="text-sm text-stone-400">{reward.description}</p>
+      <div>
+        <p className="flex items-center gap-2 text-lg font-semibold italic">
+          <FaCoins />
+          {reward.pointCost}
+        </p>
+        <div className="w-20"></div>
+      </div>
     </div>
   );
 }
