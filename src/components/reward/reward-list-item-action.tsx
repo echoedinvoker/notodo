84076fee8b444f3528eece1ab2bc0e00fc69b8c0
Matt Chang <echoedinvoker@gmail.com>
2024-12-25 19:43:50 +0800

    Update link and checkbox styling in components

--- src/components/reward/reward-list-item-action.tsx
+++ src/components/reward/reward-list-item-action.tsx
@@ -20,11 +20,11 @@ export default function RewardListItemAction({ name, userId, rewardId, ...props 
 
   return (
     <>
-      <div className="absolute peer hidden group-hover:flex inset-y-0 -right-3 w-8 flex-col justify-around items-center" {...props}>
+      <div className="absolute peer hidden group-hover:flex inset-y-0 -right-4 w-8 flex-col justify-around items-center" {...props}>
         <input type="checkbox" className="peer hidden" id={name} />
         <label
           htmlFor={name}
-          className="bg-stone-300 peer-checked:bg-stone-50 rounded-full z-40 before:content-[':'] peer-checked:before:content-['✕'] before:text-lg before:w-6 before:h-6 before:rounded-full before:flex before:items-center before:justify-center hover:before:bg-stone-200 before:duration-200"
+          className="bg-stone-300 peer-checked:bg-stone-50 rounded-full z-40 before:content-[':'] peer-checked:before:content-['✕'] before:text-lg before:w-8 before:h-8 before:rounded-full before:flex before:items-center before:justify-center hover:before:bg-stone-200 before:duration-200"
         ></label>
       </div>
       <div className="opacity-0 peer-has-[:checked]:opacity-100 duration-200 flex items-center justify-center gap-4 absolute inset-0 bg-stone-300 rounded-lg">
