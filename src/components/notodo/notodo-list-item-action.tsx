6c656f99728136672434120bfd9f7821195147ad
Matt Chang <echoedinvoker@gmail.com>
2024-12-30 18:49:27 +0800

    Add tooltips to action buttons

--- src/components/notodo/notodo-list-item-action.tsx
+++ src/components/notodo/notodo-list-item-action.tsx
@@ -12,6 +12,7 @@ interface NotodoListItemActionProps {
 export default function NotodoListItemAction({ notodo }: NotodoListItemActionProps) {
   const isCurrentChallenging = notodo.challenges.some((challenge) => !challenge.endTime)
 
+  // TODO: add tooltip to each buttons
   const links = [
     { isLink: true, href: paths.editNotodoPage(notodo.user.id, notodo.id), icon: <FaPencil /> },
     { isLink: true, href: paths.challengeListPage(notodo.user.id, notodo.id), icon: <FaFlag /> },
@@ -20,6 +21,7 @@ export default function NotodoListItemAction({ notodo }: NotodoListItemActionPro
       ? { isLink: false, notodoId: notodo.id, actionType: 'giveup', icon: <FaXmark /> }
       : { isLink: true, href: paths.deleteNotodoPage(notodo.user.id, notodo.id), icon: <FaTrash /> },
   ]
+
   return (
     <div className="absolute inset-y-0 -right-2 w-8 flex flex-col justify-around items-center">
       {links.map(({ isLink, href, notodoId, actionType, icon }, index) => (
