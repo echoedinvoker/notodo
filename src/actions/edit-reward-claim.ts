4842431590442dd340bfe1fad4a5d238895e39fc
Matt Chang <echoedinvoker@gmail.com>
2025-01-13 15:56:12 +0800

    Update editRewardClaim function for editing reward claim

--- src/actions/edit-reward-claim.ts
+++ src/actions/edit-reward-claim.ts
@@ -1,1 +1,58 @@
-// TODO: basically only update the claim content only but need to validate length of string
+'use server';
+
+import { auth } from "@/auth";
+import { db } from "@/db";
+import { paths } from "@/paths";
+import { revalidatePath } from "next/cache";
+import { redirect } from "next/navigation";
+import { z } from "zod";
+
+const editRewardClaimSchema = z.object({
+  content: z.string().max(300, "Content must be 300 characters or less"),
+});
+
+interface EditRewardClaimFormState {
+  errors: {
+    content?: string[];
+    _form?: string[];
+  }
+  success?: boolean;
+}
+
+export async function editRewardClaim(
+  rewardClaimId: string,
+  rewardId: string,
+  formState: EditRewardClaimFormState,
+  formData: FormData
+): Promise<EditRewardClaimFormState> {
+  const session = await auth();
+  if (!session || !session.user) {
+    return { errors: { _form: ["You must be logged in to edit a reward claim"] } }
+  }
+
+  let result: { content: string };
+  try {
+    result = editRewardClaimSchema.parse({
+      content: formData.get("content"),
+    })
+  } catch (error) {
+    return { errors: (error as z.ZodError).flatten().fieldErrors } as EditRewardClaimFormState;
+  }
+
+  try {
+    await db.rewardClaim.update({
+      where: { id: rewardClaimId },
+      data: {
+        content: result.content,
+      },
+    });
+  } catch (error: unknown) {
+    if (error instanceof Error) {
+      return { errors: { _form: [error.message] } };
+    }
+    return { errors: { _form: ["An unknown error occurred"] } };
+  }
+
+  revalidatePath(paths.rewardClaimShowPage(session.user.id, rewardId, rewardClaimId));
+  redirect(paths.rewardClaimShowPage(session.user.id, rewardId, rewardClaimId));
+}
