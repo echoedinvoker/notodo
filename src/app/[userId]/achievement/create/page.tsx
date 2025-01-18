ac26d32ebc5b0910482cadcf5b87d5131010e6d2
Matt Chang <echoedinvoker@gmail.com>
2025-01-18 12:43:21 +0800

    Add achievement create and list page components

---
+++ src/app/[userId]/achievement/create/page.tsx
@@ -0,0 +1,7 @@
+export default function CreateAchievementPage() {
+  return (
+    <div>
+      <h1>Create Achievement</h1>
+    </div>
+  );
+}
---
+++ src/app/[userId]/achievement/page.tsx
@@ -0,0 +1,7 @@
+export default function AchievementListPage() {
+  return (
+    <div>
+      <h1>Achievement List</h1>
+    </div>
+  );
+}
