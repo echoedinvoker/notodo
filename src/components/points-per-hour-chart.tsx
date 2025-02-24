e1b3c90ff0426581487585da633a7e6740dc5fb6
echoedinvoker <echoedinvoker@gmail.com>
2025-02-24 07:30:10 +0000

    Update left margin value to 20 in PointsPerHourChart

--- src/components/points-per-hour-chart.tsx
+++ src/components/points-per-hour-chart.tsx
@@ -76,7 +76,7 @@ export default function PointsPerHourChart({
         margin={{
           top: 5,
           right: 30,
-          left: 10,
+          left: 20,
           bottom: 5,
         }}
       >
